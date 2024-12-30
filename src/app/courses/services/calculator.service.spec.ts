06d0c19e8409b044eebaf04d6a1c7308548453bb
VagTsop <vatsop52@gmail.com>
2024-12-30 12:49:56 +0200

    Using Dependency Injection in Angular Testing - The Angular TestBed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,45 +1,47 @@
 import { CalculatorService } from "./calculator.service";
+import { TestBed } from "@angular/core/testing";
+import { LoggerService } from "./logger.service";
 
 describe("CalculatorService", () => {
-  let calculator: CalculatorService, 
-  loggerSpy: any;
 
-  // is going to be executed before each of the specifications - two times here
+  let calculator: CalculatorService, loggerSpy: any;
 
   beforeEach(() => {
-    console.log('Calling beforeEach');
+    console.log("Calling beforeEach");
+
+   
     loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
 
-    calculator = new CalculatorService(loggerSpy);
+    // used to configure our tests specks like register services for dependency injection etc
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+
+    calculator = TestBed.inject(CalculatorService); 
+    /* with TestBed our tests also based on dependency injection just like the running application.
+     
+    what is the advantage of using TestBed when compared to calling the constructor of the calculator
+    service directly?
+    */
   });
 
   it("should add two numbers", () => {
 
-    console.log('add test');
-
-    // const logger = jasmine.createSpyObj("LoggerService", ["log"]);
-
-    // const calculator = new CalculatorService(logger);
+    console.log("add test");
 
     const result = calculator.add(2, 2);
-//
+
     expect(result).toBe(4);
 
     expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract two numbers", () => {
-   
-    console.log('subtract test');
-   
-    /*
-  initialization logic has been repeated in these 2 lines - repeated code
-  for this reason we have jasmine beforeEach execution block
-    
-    const logger = jasmine.createSpyObj("LoggerService", ["log"]);
 
-    const calculator = new CalculatorService(logger);
-  */
+    console.log("subtract test");
 
     const result = calculator.subtract(2, 2);
 
@@ -48,8 +50,3 @@ describe("CalculatorService", () => {
     expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
-
-/*
-
-
-*/
