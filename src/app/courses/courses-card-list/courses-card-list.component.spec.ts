0323b7667452b3964e0a9a405ccf0f9b717f3076
VagTsop <vatsop52@gmail.com>
2025-01-03 15:21:22 +0200

    Angular Component Testing - Test Setup with the async Test Utility

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,15 +1,33 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { CoursesModule } from "../courses.module";
+import { CoursesCardListComponent } from "./courses-card-list.component";
+
+// Presentational Component - display course list
+// -----------------------------------------------
+// simple take some input data and display it on the screen
+// many times we avoid to test presentationalk components due to the simplicity of component and we go immediately to
+// container components. This is Valid approach.
+// But we might have to test them due to test coverage
+// requirements in our project that is not our control
 
 describe("CoursesCardListComponent", () => {
-  beforeEach(() => {
+  let component: CoursesCardListComponent;
+
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [CoursesModule],
-    });
-  });
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+      });
+  })); //
 
   it("should create the component", () => {
-    pending();
+    expect(component).toBeTruthy();
   });
 
   it("should display the course list", () => {
